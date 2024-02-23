3b8b92e31be2bb262db8dfb96e5120f5fb134ed9
vijay010498 <svijayr007@gmail.com>
2024-02-23 18:50:42 -0500

    add:added code to let driver create ride

--- src/driver/driver.service.ts
+++ src/driver/driver.service.ts
@@ -4,7 +4,7 @@ import {
   UnprocessableEntityException,
 } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { DriverRideDocument } from './driver-ride.schema';
+import { DriverRideDocument, GeoJSONType } from './driver-ride.schema';
 import { Model } from 'mongoose';
 import { UserVehicleDocument } from '../common/schemas/user-vehicle.schema';
 import { CreateDriverRideRequestDto } from '../common/dtos/create-driver-ride-request.dto';
@@ -35,7 +35,31 @@ export class DriverService {
       throw new UnprocessableEntityException(
         'Vehicle Not Found, Please Create new Vehicle',
       );
-    this.logger.log('rideRequestDto', rideRequestDto, user);
-    return 'Ride Created';
+
+    // create new ride
+    const driverRide = new this.driverRideCollection({
+      userId: user.id,
+      origin: {
+        type: GeoJSONType.Point,
+        coordinates: [
+          rideRequestDto.originLongitude,
+          rideRequestDto.originLatitude,
+        ],
+      },
+      destination: {
+        type: GeoJSONType.Point,
+        coordinates: [
+          rideRequestDto.destinationLongitude,
+          rideRequestDto.destinationLatitude,
+        ],
+      },
+      leaving: rideRequestDto.leaving,
+      vehicleId: rideRequestDto.vehicleId,
+      luggage: rideRequestDto.luggage,
+      emptySeats: rideRequestDto.emptySeats,
+      seatPrice: rideRequestDto.seatPrice,
+      tripDescription: rideRequestDto.tripDescription,
+    });
+    return driverRide.save();
   }
 }
